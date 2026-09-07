9d7b2acef4e80038259f25615c52a6e7ba84a1e2
jxavier <sundayjohnxavier47@gmail.com>
2026-09-07 11:11:51 +0100

    update

--- quotes.py
+++ quotes.py
@@ -1,3 +1,4 @@
+
 def fix_quotes(text):
     result = []
     double_open = True
@@ -6,7 +7,25 @@ def fix_quotes(text):
     for i in range(len(text)):
         char = text[i]
 
-        if char == '"' or "'":
-            is_double = '"'
+        if char == '"' or char ==  "'":
+            is_double = (char == '"')
             is_open = (is_double and double_open) or ( not is_double and single_open)
-            
+
+            if is_open:
+                result.append(char)
+                while i + 1 < len(text) and text[i+1] == " ":
+                    i = i + 1
+            else:
+                while len(result) > 0 and result[-1] == " ":
+                    result.pop()
+                result.append(char)
+
+            if is_double:
+                double_open = not double_open
+            else:
+                single_open = not single_open
+
+        else:
+            result.append(char)
+        
+    return "".join(result)
