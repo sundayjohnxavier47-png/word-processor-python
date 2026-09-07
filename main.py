29132603ad7f5fd3acf6d02adb05ee8c82683914
jxavier <sundayjohnxavier47@gmail.com>
2026-09-07 09:00:03 +0100

    update

--- main.py
+++
@@ -1,9 +0,0 @@
-def collapse_spaces(text):
-    result = []
-    for char in text:
-        if char == " ":
-            if len(result) > 0 and result[-1] == " ":
-                result.pop()
-                continue
-        result.append(char)
-    return "".join(result)
